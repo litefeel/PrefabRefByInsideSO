9c5d081a7b54e4520a6baf76395694e4bee7b434
litefeel <litefeel@gmail.com>
2018-10-14 09:17:42 +0800

    Initial commit

---
+++ Assets/MySO.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MySO : ScriptableObject
+{
+
+    public GameObject prefab;
+
+
+#if UNITY_EDITOR
+    [MenuItem("Tools/AddSOToPrefab")]
+    public static void AddSOToPrefab()
+    {
+        var prefab = Selection.activeObject as GameObject;
+        if (prefab)
+        {
+            var so = CreateInstance<MySO>();
+            so.name = "MySo";
+            so.prefab = prefab;
+            Debug.Log(prefab, prefab);
+            var path = AssetDatabase.GetAssetPath(prefab);
+            AssetDatabase.AddObjectToAsset(so, path);
+            AssetDatabase.SaveAssets();
+        }
+    }
+
+#endif
+}
